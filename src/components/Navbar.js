1bc6a31df206f741f30ca5458da62db5dc8d98ee
unknown <asim26k@gmail.com>
2023-09-20 02:32:39 +0530

    Navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,14 @@ const styleScript = Style_Script({ subsets: ["latin"], weight: "400" });
 const Navbar = () => {
   return (
     <div className="pt-12 flex justify-between items-center">
-      <div className="max-h-[48px] bg-red-400">
-        <img className="object-contain" src="Logo.jpg" alt="" />
+      <div className="max-h-[48px] rounded-full bg-red-400">
+        <img className="max-h-[48px] rounded-full" src="Logo.jpg" alt="" />
       </div>
       <div className={`${styleScript.className} + text-2xl text-[#A11055]`}>
         Dulcet Cupcakes
       </div>
-      <div className="h-[48px] w-[48px]">
-        <FiMenu className="text-[#A11055]  h-[48px]" />
+      <div className="h-[48px] w-[48px] ">
+        <FiMenu className="text-[#A11055]  h-[48px] w-[48px]" />
       </div>
     </div>
   );
