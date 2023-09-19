38dd9a3d100ad75c62700bc1480e396ce5b9b9d5
unknown <asim26k@gmail.com>
2023-09-20 02:20:04 +0530

    nav : incomplete

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  return <main className="text-red-400">Dulcet Cupcakes</main>;
+  return (
+    <main className="bg-[#FDEAF0] min-h-screen px-16">
+      <Navbar />
+    </main>
+  );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Style_Script } from "next/font/google";
+import { FiMenu } from "react-icons/fi";
+
+const styleScript = Style_Script({ subsets: ["latin"], weight: "400" });
+
+const Navbar = () => {
+  return (
+    <div className="pt-12 flex justify-between items-center">
+      <div className="max-h-[48px] bg-red-400">
+        <img className="object-contain" src="Logo.jpg" alt="" />
+      </div>
+      <div className={`${styleScript.className} + text-6xl text-[#A11055]`}>
+        Dulcet Cupcakes
+      </div>
+      <div className="h-[48px] w-[48px]">
+        <FiMenu className="text-[#A11055]  h-[48px]" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
