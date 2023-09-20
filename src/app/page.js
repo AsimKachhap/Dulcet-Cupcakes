ca73af74f1af37d6bc19431b1f49e9e6b4e4d387
unknown <asim26k@gmail.com>
2023-09-21 00:30:02 +0530

    Added Hero Image

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,11 @@ export default function Home() {
   return (
     <main className="bg-[#FDEAF0] min-h-screen px-4">
       <Navbar />
+      <section className="mt-8">
+        <div>
+          <img src="Hero.png" alt="" />
+        </div>
+      </section>
     </main>
   );
 }
