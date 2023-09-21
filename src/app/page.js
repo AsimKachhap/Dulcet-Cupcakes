64f70fd703292912b942d7f43ca5a0f1e806f091
unknown <asim26k@gmail.com>
2023-09-22 00:02:38 +0530

    Fixed minor issues in copy

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,12 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import { Inter } from "next/font/google";
 
 const jakarta = Plus_Jakarta_Sans({
-  weight: "600",
+  weight: "700",
   subsets: ["latin"],
 });
 
 const inter = Inter({
-  weight: "500",
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -23,17 +23,19 @@ export default function Home() {
         </div>
       </section>
       <section className=" mt-8">
-        <div className={`${jakarta.className} + text-black text-4xl`}>
+        <div
+          className={`${jakarta.className} + text-black text-3xl font-bold leading-none`}
+        >
           Cherish your Special Occasion with customized Cakes from Dulcet
           Cupcakes.
         </div>
         <div
-          className={`${inter.className} + text-slate-600 text-3xl tracking-loose mt-6`}
+          className={`${inter.className} + text-slate-600 text-2xl tracking-wide mt-6`}
         >
           We provide customized cake for every occasion in and around Ranchi,
           Jharkhand.
         </div>
-        <div className="mt-4 text-[#F8DAE8] font-semibold inline-block rounded-tl-full rounded-br-full bg-[#A11055] px-8 py-2 w-contain">
+        <div className="mt-4 text-[#F8DAE8] font-semibold inline-block rounded-md  bg-[#A11055] px-8 py-2 w-contain ">
           <button>GET YOURS NOW</button>
         </div>
       </section>
